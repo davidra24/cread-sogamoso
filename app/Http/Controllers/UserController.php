355d6438bd92eff5c81585c5503e64fbc7663ada
leidyRA <50849650+leidyRA@users.noreply.github.com>
2019-06-16 11:12:40 -0500

    Users Services
    user controller and viewUserRole creation. Modification route user in api.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,13 +1,74 @@
 <?php
 
 namespace App\Http\Controllers;
-
+Use DB;
 use Illuminate\Http\Request;
-
+use App\User;
+use App\Role;
+use App\ViewUsersRole;
 class UserController extends Controller
 {
-    function index()
+    private $user;
+    public function __construct(User $user)
     {
-        return view('user.principal');
+        $this->user = $user;
     }
+    public function index()
+    {
+        $users = ViewUsersRole::all();
+
+        return response()->json($users);
+    }
+    public function store(Request $request)
+    {   
+        
+            $UserD=User::where('email',$request->email)->first();
+            if($UserD==null){
+                $role_user = Role::where('name', 'user')->first();
+                $user = new User();
+                $user->name = $request->name;
+                $user->email = $request->email;
+                $user->password = bcrypt($request->password);
+                $user->save();
+                $user->roles()->attach($role_user);
+                return response()->json($user);
+        
+
+        }
+        
+    }
+    
+    public function show($user)
+    {
+        $userF=User::find($user);
+        return response()->json($userF);
+    }
+    public function update(Request $request, $user)
+    {
+        $data = Career::find($user);
+        if ($data == null) {
+            return null;
+        } else {
+            $data->fill($request->all());
+            $data->save();
+            
+            return response()->json($data);
+        }
+    }
+    public function destroy($user)
+    {
+        $data = User::where('id',$user)->first();
+        $role=ViewUsersRole::where('user_id',$data->id)->first();
+       
+        if($data!==null){
+            if($role->name_rol=="user"){
+                //$data->carrerSubject()->delete();
+                $role_user = Role::where('name', 'user')->first();
+                $data->roles()->detach($role_user);
+                $data->delete();
+            }
+        
+
+    }
+}
 }
